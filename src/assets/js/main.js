efcbc1a031fe9afc1cdc51ba5bb402c46d52072a
ericele <ericel123new@gmail.com>
2016-12-30 17:41:56 +0900

    Header Design

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -52,4 +52,13 @@ jQuery(document).ready(function($){
 	    });
 	    return this;
 	};
+
+	 $(window).scroll(function(){
+	  var y = $(window).scrollTop();
+	  if( y > 0 ){
+	      $("#top-shadow").css({'display':'block', 'opacity':y/20});
+	  } else {
+	      $("#top-shadow").css({'display':'block', 'opacity':y/20});
+	  }
+	 });
 });
